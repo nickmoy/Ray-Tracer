1fb3f658a927048dcd91b24eb58c69750356be23
nickmoy <nickm2373@gmail.com>
2024-05-16 00:15:49 -0700

    Before moving functions to utils

---
+++ Makefile
@@ -0,0 +1,36 @@
+CC = g++
+C++STD = -std=c++17
+BUILD_FLAGS = -I Include -L Lib -l SDL2-2.0.0 -framework OpenGL
+COMPILE_FLAGS = -I Include
+
+
+play.out: build/main.o build/window.o build/renderer.o build/physics.o
+	${CC} ${C++STD} build/main.o build/window.o build/renderer.o build/physics.o -o play.out ${BUILD_FLAGS}
+	@chmod u+x play.out
+	@echo ">> Build successful!"
+
+build/main.o: src/main.cpp
+	${CC} ${C++STD} -c src/main.cpp -o main.o ${COMPILE_FLAGS}
+	@mv main.o build
+	@echo ">> main.o object file created"
+
+build/window.o: src/window.cpp
+	${CC} ${C++STD} -c src/window.cpp -o window.o ${COMPILE_FLAGS}
+	@mv window.o build
+	@echo ">> window.o object file created"
+
+build/renderer.o: src/renderer.cpp
+	${CC} ${C++STD} -c src/renderer.cpp -o renderer.o ${COMPILE_FLAGS}
+	@mv renderer.o build
+	@echo ">> renderer.o object file created"
+
+build/physics.o: src/physics.cpp
+	${CC} ${C++STD} -c src/physics.cpp -o physics.o ${COMPILE_FLAGS}
+	@mv physics.o build
+	@echo ">> physics.o object file created"
+
+clean:
+	@rm -rf build/*
+	@test -f ./play.out && rm play.out || true
+	@echo ">> Project cleaned!"
+
---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+//
+//  main.cpp
+//  Raytrace
+//
+//  Created by Nicholas Moy on 5/15/24.
+//
+
+#define GL_SILENCE_DEPRECATION
+
+#include <SDL2/SDL.h>
+#include <OpenGL/gl3.h>
+#include <glm/glm.hpp>
+#include <iostream>
+#include <format>
+#include "physics.hpp"
+#include "renderer.hpp"
+#include "window.hpp"
+
+#define FPS 60
+const float FRAME_DELTA_MIL_SEC = 1000.0f/60;   // Fixed update FPS
+const float TIME_STEP_DELTA = 1.0f/60;          // Fixed Game logic delta time
+
+int main(int argc, char *argv[])
+{
+    // Main loop objects
+    SDL_Event e;
+    bool running = true;
+    unsigned int a = SDL_GetTicks();            // Real time update guard
+    unsigned int b = SDL_GetTicks();            // Game logic time guard
+    unsigned int lastRender = SDL_GetTicks();
+    
+    Window window = Window();
+    Renderer renderer = Renderer();
+    Physics physics = Physics();
+    
+    window.addRenderer(&renderer);
+    renderer.addPhysics(&physics);
+    
+    glm::vec4 Light_Gray = glm::vec4(210.0f/255, 222.0f/255, 228.0f/255, 1.0);
+    glClearColor(Light_Gray.x, Light_Gray.y, Light_Gray.z, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    SDL_GL_SwapWindow(window);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    std::cout << "Game started!";
+    
+    // Main program loop
+    while(running)
+    {
+        // Cap Framerate
+        a = SDL_GetTicks();
+
+        // If Game logic isn't ready to update again since
+        // last frame update, wait
+        if(a < b)
+        {
+            SDL_Delay(2);
+        }
+        else
+        {
+            // Game logic is now behind current time, so we update
+            // Game logic until it is ahead of time
+            while(a > b)
+            {
+                // Poll events
+                window.PollIO(e, &running);
+                
+                /* Game physics */
+                // physics.update(TIME_STEP_DELTA);
+                
+                b += FRAME_DELTA_MIL_SEC;
+            }
+            
+            // Render
+            renderer.render();
+            
+            // Update screen
+            SDL_GL_SwapWindow(window);
+            glClear(GL_COLOR_BUFFER_BIT);
+        }
+    }
+
+    SDL_Quit();
+    return 0;
+}
+
